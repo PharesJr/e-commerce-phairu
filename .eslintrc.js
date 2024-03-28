de7c396356ebd70fc6d10c6c028897ecc159ec7d
Phares Mwangi <pharesirungu@gmail.com>
2024-03-28 22:10:24 +0300

    ESlint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,17 @@
 module.exports = {
   root: true,
-  extends: ['plugin:@next/next/recommended', '@payloadcms'],
+  extends: [
+    'plugin:@next/next/recommended',
+    '@payloadcms',
+    'next',
+    'prettier',
+    'next/core-web-vitals',
+  ],
   ignorePatterns: ['**/payload-types.ts'],
   plugins: ['prettier'],
   rules: {
-    'prettier/prettier': ['error', {endOfLine: 'auto' }],
-    "simple-import-sort/imports": "off",
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
+    'simple-import-sort/imports': 'off',
+    '@next/next/no-page-custom-font': 'off',
   },
 }
